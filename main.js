87a859de4d538685ad83ff64db22341f52e11276
James K Brooks <brooks94@mctc.edu>
2024-01-22 13:56:04 -0500

    testing and demoing git bash for students

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello, world!");
+
+let myName = "Dwayne Johnson";
+
+myName = "The Rock";
+
+console.log(myName);
+
+let totalCost;
+let totalWithTax;
+
+let costPerItem = 2.50;
+let numberOfItems = 4;
+let taxRate = 0.06;
+
+totalCost = costPerItem * numberOfItems;
+totalWithTax = totalCost + (taxRate * totalCost);
+
+console.log(totalWithTax);
